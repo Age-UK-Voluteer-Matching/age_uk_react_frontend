f546b7400a93a6647826d2d850caf5c3fdd9a8cd
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-06-15 22:35:26 +0100

    imported landing page component into app.js with dummy hello world text

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import Landing from './components/landing/Landing'
 
 function App() {
   return (
     <div className="App">
-
+      <Landing />
     </div>
   );
 }
---
+++ src/components/landing/Landing.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Landing extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello world and Welcome to AGE UK Volunteer Matching</h1>
+      </div>
+    )
+  }
+}
+
+export default Landing
