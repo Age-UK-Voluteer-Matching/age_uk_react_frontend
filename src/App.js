d40fdb586fba0fcdf2e50519480c7c2a7cb12806
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-06-16 15:44:57 +0100

    added login component to routes. includes event handlers/state/form

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Landing from './components/landing/Landing';
 import Navbar from './components/layouts/Navbar'
 import SignUpVolunteer from './components/authentication/SignUpVolunteer'
 import SignUpUser from './components/authentication/SignUpUser'
+import SignIn from './components/authentication/SignIn'
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route exact path='/' component={Landing}/>
           <Route path='/signupvolunteer' component={SignUpVolunteer}/>
           <Route path='/signupuser' component={SignUpUser}/>
+          <Route path='/login' component={SignIn}/>
         </Switch>
       </div>
     </BrowserRouter>
