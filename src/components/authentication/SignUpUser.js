c55a61f4ae4d9796037ac2eaf3c461ec12c2a354
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-06-16 15:27:58 +0100

    added state and event handlers/form to sign up user component.

--- src/components/authentication/SignUpUser.js
+++ src/components/authentication/SignUpUser.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react'
-
+import { Link } from 'react-router-dom'
 export class SignUpUser extends Component {
+  state = {
+    email: '',
+    password: ''
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state)
+  }
   render() {
     return (
       <div>
-        <h1>This is the SignUpUser component</h1>
+        <h1>Thank you for signing up to find a volunteer.</h1>
+        <h4>If you are a volunteer, please click <Link to="/signupvolunteer">Here</Link></h4>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group" >
+            <label htmlFor="exampleInputEmail1">Email address</label>
+            <input type="email" className="form-control" id="email" placeholder="Enter email" onChange={this.handleChange}/>
+            <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleInputPassword1">Password</label>
+            <input type="password" className="form-control" id="password" placeholder="Password" onChange={this.handleChange}/>
+          </div>
+          <div className="form-check">
+            <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+            <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+          </div>
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
       </div>
     )
   }
